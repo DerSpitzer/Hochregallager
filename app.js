ca2c89bfd1e63a4d46c58e468695ad9b185b38fe
leonspitzer <leonspitzer@users.noreply.github.com>
2021-06-30 16:35:12 +0200

    changes (david)

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 /* 
- * Author: Us
+ * Author: Anne Tretter
+ *
  * We will have a global variable which will tell us our time in our simulation
  * it will be possible to reset this variable.
  */
 
+// Idex of our timeline (seconds)
 let time_of_simulation = 0;
+
+// The interval itself so we can toggle it from everywhere
 var our_simulation;
+
+var xValuesLine = [0]
+var yValuesLine = [0];
 
 // This is our array of items in the storage
 var items = [ 
@@ -20,30 +27,29 @@ var items = [
         , { id: "3_3", status: "empty", RFID:111111, ID:000000, color:"white" }
     ];
 
+// Event Listener for start button
 function startSimulation() {
-    our_simulation = setInterval(simulate, 1000);
+    our_simulation = setInterval(simulate, 300);
     document.getElementById("start_button").disabled = true;
     document.getElementById("start_button").style.backgroundColor = 'RoyalBlue';
 }
 
-
+// Event listener for reset button
 function resetSimulation() {
     let reset_time = document.getElementById("reset_button");
     clearInterval(our_simulation);
     time_of_simulation = 0;
     simulate()
-
     document.getElementById("start_button").disabled = false;
     document.getElementById("start_button").style.backgroundColor = 'DodgerBlue';
-    
 }
 
-
-// return true if in range, otherwise false
+// Helper function to return true if in range, otherwise false
 function inRange(x, min, max) {
     return ((x-min)*(x-max) <= 0);
 }
 
+// Event listener for fillAll button
 function fillAll() {
     for (var x = 0; x < items.length; x += 1) {
         items[x].RFID = 55555;
@@ -55,10 +61,12 @@ function fillAll() {
         document.getElementById(items[x].id).style.backgroundColor = items[x].color;
         document.getElementById("grid" + items[x].id).style.backgroundColor = "orange";
         document.getElementById("status" + items[x].id).innerHTML = "i";
+
+        updateDoughnut();
     }
 }
 
-
+// This function checks if a fach is hit and draws our graph
 function setValues(data) {
     vertikal = data[time_of_simulation].werte["H-vertikal"];
     horizontal = data[time_of_simulation].werte["H-horizontal"];
@@ -114,7 +122,7 @@ function setValues(data) {
     ctx.fill();
 }
 
-// This function is our time updater, so we know what time it is
+// This function is our simulation (it gets called every 1000ms)
 function simulate() {
     d3.json("https://it2wi1.if-lab.de/rest/ft_fach").then( (data, error) => {
         if (error) {
@@ -129,11 +137,20 @@ function simulate() {
                 document.getElementById('time_label').innerHTML = datum;
                 setValues(data)
                 time_of_simulation += 1;
+
+                updateLinechartValues(data);
+
+                // Dealing with updating the line chart
+                if (time_of_simulation % 60 == 0) {
+                    updateLinechart();
+                    clearLineChart();
+                }
             }
         }
     });
 }
 
+// Helper function which converts an id (1_2) into the space in the array of items
 function getPos(id){
     var a = parseInt(id.slice(0,1))
     var b = parseInt(id.slice(2,3))
@@ -141,13 +158,13 @@ function getPos(id){
     return (a-1)*3+b-1;
 }
 
+// ***** The following section is dedicated to handle all window openings and closings **** //
+
 function openInfo(id) {
     if (document.getElementById("status" + id).textContent == "i") {
         document.getElementById("InfoFach").style.display = "inline-block";
         document.getElementById("headerInfoFach").innerHTML = "Info Fach " + id;
 
-       
- 
         // Set RFID and ID and the color of the blob
         var pos = getPos(id);
         document.getElementById("InfoFach-rfidLabel").innerHTML = "RFID-Adresse: " + items[pos].RFID;
@@ -172,6 +189,8 @@ function closeInfoFach() {
     document.getElementById("InfoFach").style.display = "none";
 }
 
+
+// Event listener for the submit button to submit a item
 function checkInfo() {
     // We let our window disappear
     document.getElementById("infoBox").style.display = "none";
@@ -192,8 +211,7 @@ function checkInfo() {
     if (color == "Blau") color = "blue";
     if (color == "Rot") color = "red";
     
-
-    // Make checks for the rightness //
+    // Make checks for the rightness
     if (!Number.isInteger(rfid) || rfid > 999999 || rfid < 100000 || !Number.isInteger(idnumber) || idnumber > 999999 || idnumber < 100000) {
         // There was an error, closing the info fach and opening the error message
         document.getElementById("InfoFach").style.display = "none";
@@ -201,10 +219,17 @@ function checkInfo() {
     } else {
         // Edit our array of items and display it
         changeItem(id, idnumber, rfid, color)
+
+
+        document.getElementById("Ergebnismeldung-label").innerHTML = "Die Ware (ID: " + idnumber + " / RDIF: " + rfid + ")"
+        document.getElementById("Ergebnismeldung-label2").innerHTML = "wird in das Fach " + id + " eingelagert."
+
+        // Give rfid, id, fach to the next window
         document.getElementById("Ergebnismeldung").style.display = "inline-block";
     }
 }
 
+// Assigns the given parameters to the item with id 'id'
 function changeItem(id, idnumber, rfid, color) {
     // Get the position of our element in the array
     var pos = getPos(id);
@@ -232,12 +257,25 @@ function closeErgebnismeldung(){
     document.getElementById("Ergebnismeldung").style.display = "none";
 }
 
+// Gets called once when the body loads
 function setup() {
     updateDoughnut();
+    updateLinechart();
+    initLinechart();
 }
 
+function initLinechart() {
+    for(var i=1 ; i<61 ; i++) {
+        xValuesLine.push(i);
+        yValuesLine.push(Math.floor(Math.random() * 6) + 1)
+    }
+
+    updateLinechart();
+}
+
+// Updated the doughnut chart
 function updateDoughnut() {
-    // Getting the number of items in our list
+    // Getting the number of items in our storage
     var count = 0
     for (var i in items) {
         if (items[i].status == "full") {
@@ -251,7 +289,7 @@ function updateDoughnut() {
     var yValues = [count, 9-count];
     var barColors = [
     "orange",
-    "lightgrey"
+    "green"
     ];
     new Chart("myChart", {
         type: "doughnut",
@@ -266,10 +304,72 @@ function updateDoughnut() {
             title: {
                 display: false,
             },
+            responsive: false,
         }
     });
 }
 
+function clearLineChart() {
+    var count = 0;
+    for (var i in items) {
+        if (items[i].status == "full") {
+           count+=1;
+        }
+    }
+
+    xValuesLine = [0]
+    yValuesLine = [count]
+}
+
+function updateLinechartValues(data) {
+    var count = 0;
+    for (var i in items) {
+        if (items[i].status == "full") {
+           count+=1;
+        }
+    }
+
+    var datum = data[time_of_simulation].datum;
+    datum = datum.slice(14,19);
+    xValuesLine.push(datum);
+    yValuesLine.push(count);
+}
+
+// Updated the line chart
+function updateLinechart() {
+    var barColors = [
+    "orange",
+    ];
+    new Chart("myLinechart", {
+        type: "line",
+        data: {
+            labels: xValuesLine,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yValuesLine
+            }]
+        },
+        options: {
+            title: {
+                display: false,
+            },
+            responsive: false,
+        },
+        column: {
+            pointPadding: 0.2,
+            size: '95%',
+            borderWidth: 0,
+            events: {
+                legendItemClick: function () {
+                    return false; 
+                }
+            }
+        },
+        allowPointSelect: false,
+    });
+}
+
+// Removes items from a fach
 function kickItem(getID) {
     if (getID != null) {
         var id = getID
@@ -291,21 +391,7 @@ function kickItem(getID) {
     document.getElementById("InfoFach").style.display = "none";
 }
 
-
+// Reset the simulation time
 function reset() {
     time_of_simulation = 0;
 }
-
-
-
-/*
-<button class="btn">
-            <i class="fa fa-check"></i>
-        </button>
-        <button class="btn">
-            <i class="fa fa-arrows-alt-h"></i>
-        </button>
-        <button class="btn">
-            <i class="fa fa-times"></i>
-        </button>
-        */
